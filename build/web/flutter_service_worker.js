021a75c2cb1dbbb14507fca66eaa86645a77bbbd
aliyafarkhshatova <liyafarapp@gmail.com>
2025-04-15 17:00:11 +0500

    Fix gray screen issue

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e1712625b352edadc98658db43d7a87e",
+const RESOURCES = {"flutter_bootstrap.js": "df03baf95a157f637d1cf1b2a7b55453",
 "version.json": "013e373d36705a151d2ac0cc06438b36",
 "index.html": "92c8e97f7317776e9ee52a0fc128bbf3",
 "/": "92c8e97f7317776e9ee52a0fc128bbf3",
-"main.dart.js": "a6e192f3b6142cf0284c53f3b4625067",
+"main.dart.js": "cf4313f37399afc3b46ff7dc96c65dc7",
 "img.png": "e6542037eef4145ca2ee16134573d64e",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
